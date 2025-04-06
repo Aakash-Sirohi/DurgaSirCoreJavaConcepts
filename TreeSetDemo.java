afc2e7eaf8906c431b6b7e62e5111a37585a164a
Aakash <aakashsrh@gmail.com>
2025-04-06 09:13:32 +0530

    TreeSet

--- TreeSetDemo.java
+++ TreeSetDemo.java
@@ -1,2 +1,15 @@
+import java.util.TreeSet;
+
 public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet();
+        ts.add("A");
+        ts.add("a");
+        ts.add("B");
+        ts.add("L");
+        ts.add("Z");
+//        ts.add(new Integer(10)); // In this case we will get  ClassCastException as this is addition of Heterogeneous Object
+//        ts.add(null);            // In this case we will get NullPointerException
+        System.out.println(ts);
+    }
 }
