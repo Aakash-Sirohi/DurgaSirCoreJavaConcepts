3d276b4f452d7298187f5346e5e367f1fe34e8e0
Aakash <aakashsrh@gmail.com>
2025-05-02 06:11:59 +0530

    Chapter 1 - Language Fundamentals

---
+++ Language_Fundamentals/JavaIdentifiersDemo.java
@@ -0,0 +1,34 @@
+package Language_Fundamentals;
+
+// JavaIdentifiersDemo is an identifier
+public class JavaIdentifiersDemo {
+    // main is an identifier
+    // String is an identifier
+    // args is an identifier
+    public static void main(String[] args) {
+        // x is an identifier
+        int x = 10;
+
+        // int total_number; ==> valid identifier
+        // int total#; ==> invalid identifier
+        // int total123; ==> valid identifier
+        // int 123total; ==> invalid identifier
+
+        // we can take any length identifier in java
+        // It is recommended to not take too lengthy identifiers
+        int xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz= 44;
+
+
+        // we cannot take reserved words as identifiers
+        // int if = 20;
+
+        // All pre-defined java class names and interfaces are valid identifiers
+        // Even though it is valid, but it is not a good practice to use them as identifiers
+
+        int String = 10;
+        System.out.println(String);
+
+        int Runnable = 22;
+        System.out.println(Runnable);
+    }
+}
